8b2b1acbe91a34d7709b4046a0db2521dbc0a4eb
grovergonzalez <grover.gonzalez@ucb.edu.bo>
2023-04-03 22:29:07 -0400

    Se ingresan las coordenadas 2,2 y se muestra en pantalla

--- src/controlador.test.js
+++ src/controlador.test.js
@@ -1,14 +1,16 @@
-function controlar_auto(x, y){
+function mostrar_posicion_automovil(x, y){
   var inicio = x + "," + y;
   return inicio;
 }
 
-
 describe("Controlar", () => {
   it("se ingresan coordenas 0,0 y se muestra las coordenadas", () => {
-    expect(controlar_auto(0, 0)).toEqual("0,0");
+    expect(mostrar_posicion_automovil(0, 0)).toEqual("0,0");
   });
   it("se ingresan coordenas 1,1 y se muestra las coordenadas", () => {
-    expect(controlar_auto(1, 1)).toEqual("1,1");
+    expect(mostrar_posicion_automovil(1, 1)).toEqual("1,1");
+  });
+  it("se ingresan coordenas 2,2 y se muestra las coordenadas", () => {
+    expect(mostrar_posicion_automovil(2, 2)).toEqual("2,2");
   });
 });
