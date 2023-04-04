ad250ff69fa67100a66eeef6b2071bae8254a02c
grovergonzalez <grover.gonzalez@ucb.edu.bo>
2023-04-03 22:30:21 -0400

    Se muestra el carro en punto origen y se manda los comandos de movimiento

--- src/controlador.test.js
+++ src/controlador.test.js
@@ -2,6 +2,33 @@ function mostrar_posicion_automovil(x, y){
   var inicio = x + "," + y;
   return inicio;
 }
+function separar_Datos(texto) {
+  return texto.split('/');
+}
+
+function obtener_Coordenadas(texto) {
+  const partes = texto.split(',');
+  if (partes.length !== 2) {
+    throw new Error('Formato inválido');
+  }
+
+  const x = parseInt(partes[0]);
+  const y = parseInt(partes[1].substring(0, partes[1].length - 1));
+  const orientacion = partes[1].substring(partes[1].length - 1);
+
+  return { x, y, orientacion };
+}
+
+function controlar_auto(comandos){
+  const datos = separar_Datos(comandos) //["5,5", "1,2N", "IAIAIAIAA"]
+  const coordenadas = datos[1];
+  const valores = obtener_Coordenadas(coordenadas);
+  const x = valores.x;
+  const y = valores.y;
+  const orientacion = valores.orientacion;
+
+  return x + "," + y+ orientacion;
+}
 
 describe("Controlar", () => {
   it("se ingresan coordenas 0,0 y se muestra las coordenadas", () => {
@@ -13,4 +40,8 @@ describe("Controlar", () => {
   it("se ingresan coordenas 2,2 y se muestra las coordenadas", () => {
     expect(mostrar_posicion_automovil(2, 2)).toEqual("2,2");
   });
+  it("se ingresa el tamanio del mapa, punto de origen y comandos de movimiento, y muestra el punto de origen", () => {
+    expect(controlar_auto("5,5/1,2N/IAIAIAIAA")).toEqual("1,2N");
+  });
+
 });
