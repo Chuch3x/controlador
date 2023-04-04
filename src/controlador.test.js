a2124a9dc685e49849f4cb556868903fed231393
Cesar <c.augusto_mendez@hotmail.com>
2023-04-03 22:26:45 -0400

    Se ingresan coordenada 1,1 y se retorna

--- src/controlador.test.js
+++ src/controlador.test.js
@@ -1,10 +1,14 @@
 function controlar_auto(x, y){
-  return "0,0";
+  var inicio = x + "," + y;
+  return inicio;
 }
 
 
 describe("Controlar", () => {
   it("se ingresan coordenas 0,0 y se muestra las coordenadas", () => {
     expect(controlar_auto(0, 0)).toEqual("0,0");
+  });
+  it("se ingresan coordenas 1,1 y se muestra las coordenadas", () => {
+    expect(controlar_auto(1, 1)).toEqual("1,1");
   });
 });
