3503132da241aeab045d0bcd2c64fae31dd3199e
Grover <grover.gonzalez@ucb.edu.bo>
2023-04-09 17:43:46 -0400

    Utilizando solo la clase auto en todos los tests y las funciones

--- src/controlador.js
+++ src/controlador.js
@@ -1,7 +1,3 @@
-var x = 0;
-var y = 0;
-var orientacion = "";
-
 class Auto {
   constructor(posX, posY, orientacion) {
     this.posX = posX;
@@ -13,103 +9,91 @@ class Auto {
     return x + "," + y;
   }
 
-  CambiarOrientacion() {
+  separar_Datos(texto){
+    return texto.split("/");
   }
 
-  CambiarPosicion(NewX, NewY) {
-    return this.MostrarAuto(NewX, NewY);
+  obtener_Coordenadas(texto) {
+    const partes = texto.split(",");
+    if (partes.length !== 2) {
+      throw new Error("Formato inválido");
+    }
+  
+    const x = parseInt(partes[0]);
+    const y = parseInt(partes[1].substring(0, partes[1].length - 1));
+    const orientacion = partes[1].substring(partes[1].length - 1);
+  
+    return { x, y, orientacion };
   }
 
-}
-
-function mostrar_posicion_automovil(x, y) {
-  Auto.MostrarAuto(x, y);
-}
-function separar_Datos(texto) {
-  return texto.split("/");
-}
-
-function obtener_Coordenadas(texto) {
-  const partes = texto.split(",");
-  if (partes.length !== 2) {
-    throw new Error("Formato inválido");
+  get_coordenadas(comandos) {
+    const datos = this.separar_Datos(comandos); //["5,5", "1,2N", "IAIAIAIAA"]
+    const coordenadas = datos[1];
+    const valores = this.obtener_Coordenadas(coordenadas);
+    const x = valores.x;
+    const y = valores.y;
+    const orientacion = valores.orientacion;
+  
+    return { x: x, y: y, orientacion: orientacion };
   }
 
-  const x = parseInt(partes[0]);
-  const y = parseInt(partes[1].substring(0, partes[1].length - 1));
-  const orientacion = partes[1].substring(partes[1].length - 1);
+  get_mapa(comandos) {
+    const datos = this.separar_Datos(comandos);
+    const mapa = datos[0];
+    return mapa;
+  }
 
-  return { x, y, orientacion };
-}
+  get_comandos(comandos) {
+    const datos = this.separar_Datos(comandos); //["5,5", "1,2N", "IAIAIAIAA"]
+    const comandos_mov = datos[2];
+  
+    return comandos_mov;
+  }
 
-function get_coordenadas(comandos) {
-  const datos = separar_Datos(comandos); //["5,5", "1,2N", "IAIAIAIAA"]
-  const coordenadas = datos[1];
-  const valores = obtener_Coordenadas(coordenadas);
-  const x = valores.x;
-  const y = valores.y;
-  const orientacion = valores.orientacion;
-
-  return { x: x, y: y, orientacion: orientacion };
-}
-
-function get_comandos(comandos) {
-  const datos = separar_Datos(comandos); //["5,5", "1,2N", "IAIAIAIAA"]
-  const comandos_mov = datos[2];
-
-  return comandos_mov;
-}
-
-function get_mapa(comandos) {
-  const datos = separar_Datos(comandos);
-  const mapa = datos[0];
-
-  return mapa;
-}
-
-function ejecutarComandos(comandos){
-  let matrizDimension = get_mapa(comandos);
-  console.log(matrizDimension)
-  let resultado = get_coordenadas(comandos);
-  let posX = resultado.x;
-  let posY = resultado.y;
-  let direccion = resultado.orientacion;
-
-  for (let i = 0; i < comandos.length; i++) {
-    let comando = comandos[i];
-    if (comando === "I") {
-      if (direccion === "N") {
-        direccion = "O";
-      } else if (direccion === "O") {
-        direccion = "S";
-      } else if (direccion === "S") {
-        direccion = "E";
-      } else if (direccion === "E") {
-        direccion = "N";
-      }
-    } else if (comando === "D") {
-      if (direccion === "N") {
-        direccion = "E";
-      } else if (direccion === "E") {
-        direccion = "S";
-      } else if (direccion === "S") {
-        direccion = "O";
-      } else if (direccion === "O") {
-        direccion = "N";
-      }
-    } else if (comando === "A") {
-      if (direccion === "N" && posY < matrizDimension[0]) {
-        posY++;
-      } else if (direccion === "E" && posX < matrizDimension[0]) {
-        posX++;
-      } else if (direccion === "S" && posY > 0) {
-        posY--;
-      } else if (direccion === "O" && posX > 0) {
-        posX--;
+  ejecutarComandos(comandos){
+    let matrizDimension = this.get_mapa(comandos);
+    console.log(matrizDimension)
+    let resultado = this.get_coordenadas(comandos);
+    let posX = resultado.x;
+    let posY = resultado.y;
+    let direccion = resultado.orientacion;
+  
+    for (let i = 0; i < comandos.length; i++) {
+      let comando = comandos[i];
+      if (comando === "I") {
+        if (direccion === "N") {
+          direccion = "O";
+        } else if (direccion === "O") {
+          direccion = "S";
+        } else if (direccion === "S") {
+          direccion = "E";
+        } else if (direccion === "E") {
+          direccion = "N";
+        }
+      } else if (comando === "D") {
+        if (direccion === "N") {
+          direccion = "E";
+        } else if (direccion === "E") {
+          direccion = "S";
+        } else if (direccion === "S") {
+          direccion = "O";
+        } else if (direccion === "O") {
+          direccion = "N";
+        }
+      } else if (comando === "A") {
+        if (direccion === "N" && posY < matrizDimension[0]) {
+          posY++;
+        } else if (direccion === "E" && posX < matrizDimension[0]) {
+          posX++;
+        } else if (direccion === "S" && posY > 0) {
+          posY--;
+        } else if (direccion === "O" && posX > 0) {
+          posX--;
+        }
       }
     }
-}
-  return posX + "," + posY + direccion;
+    return posX + "," + posY + direccion;
+  }
 }
 
-export {get_comandos, get_coordenadas, obtener_Coordenadas, separar_Datos, mostrar_posicion_automovil, Auto, ejecutarComandos}
+export {Auto}
--- src/controlador.test.js
+++ src/controlador.test.js
@@ -1,4 +1,4 @@
-import {Auto, get_comandos, get_coordenadas, obtener_Coordenadas, separar_Datos, mostrar_posicion_automovil, ejecutarComandos} from "./controlador.js";
+import {Auto} from "./controlador.js";
 
 let auto = new Auto(0,0,"N");
 
@@ -13,13 +13,13 @@ describe("Controlar", () => {
     expect(auto.MostrarAuto(2, 2)).toEqual("2,2");
   });
   it("se ingresa el tamanio del mapa, punto de origen y comandos de movimiento, y muestra el punto de origen", () => {
-    expect(get_coordenadas("5,5/1,2N/IAIAIAIAA")).toEqual({"orientacion": "N", "x": 1, "y": 2});
+    expect(auto.get_coordenadas("5,5/1,2N/IAIAIAIAA")).toEqual({"orientacion": "N", "x": 1, "y": 2});
   });
   it("se ingresa el tamanio del mapa, punto de origen y comandos de movimiento, y muestra los comandos de movimiento", () => {
-    expect(get_comandos("5,5/1,2N/IAIAIAIAA")).toEqual("IAIAIAIAA");
+    expect(auto.get_comandos("5,5/1,2N/IAIAIAIAA")).toEqual("IAIAIAIAA");
   });
   it("Se mueve el carro a la posicion esperada segun los movimientos", () => {
-   expect(ejecutarComandos("5,5/3,3E/AADAADADDA")).toEqual("5,1E");
+   expect(auto.ejecutarComandos("5,5/3,3E/AADAADADDA")).toEqual("5,1E");
   });
 });
 
