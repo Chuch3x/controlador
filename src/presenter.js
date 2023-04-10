d27a332623e0784218302993c669e200e344c8de
Cesar <c.augusto_mendez@hotmail.com>
2023-04-10 19:42:47 -0400

    Implementando toda la funcionalidad en la pagina html

--- src/presenter.js
+++ src/presenter.js
@@ -1,17 +1,19 @@
-import {get_comandos, get_coordenadas, obtener_Coordenadas, separar_Datos, mostrar_posicion_automovil} from "./controlador"
+import {get_comandos, Auto, get_coordenadas, obtener_Coordenadas, separar_Datos, mostrar_posicion_automovil} from "./controlador"
 
 const comandos = document.querySelector("#comandos_input");
 const form = document.querySelector("#controlador_form");
 const div = document.querySelector("#resultado_div");
 
+let auto = new Auto(0,0,"N");
+
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   console.log(comandos.value);
-
-  var comandos_usuario = get_comandos(comandos.value);
-  console.log(comandos_usuario);
-  console.log(typeof(comandos_usuario)); //tipo string
+  let respuesta = auto.ejecutarComandos(comandos.value);
+  //var comandos_usuario = get_comandos(comandos.value);
+  //console.log(comandos_usuario);
+  //console.log(typeof(comandos_usuario)); //tipo string
   
-  div.innerHTML = "<p>" + comandos_usuario + "</p>";
+  div.innerHTML = "<p>" + respuesta + "</p>";
 
 });
